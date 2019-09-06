4c243a4919197db3e4969b9210e908fad5272cb7
CJosephW <wisecameron1@gmail.com>
2019-09-06 09:56:19 -0400

    Gathered shift time, location, and date
    to-do: close modal pop ups

--- auto_pickup.py
+++ auto_pickup.py
@@ -1,11 +1,11 @@
 import time
 from selenium import webdriver
-driver = webdriver.Firefox(firefox_binary="/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox-bin")
+driver = webdriver.Firefox()
 
 driver.get("https://www.hotschedules.com/hs/login.jsp")
 
-usr =  input('please enter username')
-pas = input('please enter password')
+usr =  'cameron.wise'
+pas = 'april82002'
 
 username = driver.find_element_by_id('loginusername')
 username.clear()
@@ -15,17 +15,39 @@ password = driver.find_element_by_id('loginpassword')
 password.clear()
 password.send_keys(pas)
 
+
+shift_times = []
+shift_dates = []
+shift_locations = []
+
 driver.find_element_by_id('loginBtn').click()
 
-driver.get('https://www.hotschedules.com/hs/menuParser.hs?screen=employeeHome&firstTime=true#/home?startDate=2019-08-27&showPickup=true')
+driver.get('https://www.hotschedules.com/hs/menuParser.hs?screen=employeeHome&firstTime=true#/home?startDate=2019-09-06&showPickup=true')
 
 time.sleep(5)
 
 while len(driver.find_elements_by_css_selector('.spinner')) > 0:
     time.sleep(1)
 
-print(driver.find_elements_by_css_selector('div.shift-pickup-item'))
 for items in driver.find_elements_by_css_selector('div.shift-pickup-item'):
     items.click()
-    print(driver.find_elements_by_css_selector('strong.shift-info').text)
+    
+    date = driver.find_element_by_css_selector('div.echo-component-EchoText-10vh5.echo-component-EchoText-3Pr7a.echo-component-EchoText-loCSL.echo-component-EchoText-3YRM3.echo-component-EchoText-2AVq7.echo-component-EchoText-2ZP5r.echo-component-EchoText-WQy-s')
+    shift_location = driver.find_element_by_css_selector('div.table-item.location-name.pickup-table')
+    
+    shift_locations.append(shift_location.text)
+    
+    shift_dates.append(date.text[28:])
+    
+    times = driver.find_elements_by_css_selector('div.table-item.shift-time')
+    
+    
+    for time in times:
+        if time.text != 'SHIFT TIME':
+            shift_times.append(time.text)
+    driver.find_element_by_id("//use[@href='#iso-close']").click()
+    
+    
 
+for shift in shift_times:
+    print(shift)
